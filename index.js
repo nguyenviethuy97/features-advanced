b1d8a22faee4bc82c3347043f67c30f330cb4a80
nguyenviethuy97 <tuwnguyen@gmail.com>
2023-02-28 16:47:07 +0700

    set up

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const keys = require('./config/keys');
+
+require('./models/User');
+require('./models/Blog');
+
+//handling authentication
+require('./services/passport');
+require('./services/cache');
+
+//mongoose instance
+mongoose.Promise = global.Promise;
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
+
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+//events handlers
+require('./routes/authRoutes')(app);
+require('./routes/blogRoutes')(app);
+require('./routes/uploadRoutes')(app);
+
+//production file serving
+if (['production', 'ci'].includes(process.env.NODE_ENV)) {
+  app.use(express.static('client/build'));
+
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve('client', 'build', 'index.html'));
+  });
+}
+
+//start app listening on given port
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Listening on port`, PORT);
+});
